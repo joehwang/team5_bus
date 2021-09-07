d61a0be4456217a3a054cc54811486e5b56e6e30
joehwang <joehwang.com@gmail.com>
2021-09-07 11:41:58 +0800

    Update notification.rb

--- src/notification.rb
+++ src/notification.rb
@@ -2,40 +2,35 @@ require 'httparty'
 require 'json'
 
 class Notification
-	attr_accessor :notify_email
-	
-	def initialize(_email)
-		@notify_email = _email
-	end
+  attr_accessor :notify_email
 
-	def invoke(_msg)
-		HTTParty.post("https://mail.surenotifyapi.com/v1/messages", 
-		body: {
-			subject: "#{_msg}",
-			fromName: "公車通知",
-			fromAddress: "joehwang.com@gmail.com",
-			content: "<html><body><h3>#{_msg}</h3></body></html>",
-			unsubscribedLink: "https://links.to.your.unsubscribe.link",
-			recipients: [
-				{
-					name: "#{@notify_email}",
-					address: "#{@notify_email}",
-					variables: {
-						UUID: "1234-56-78-9012"
-					}
-				}
-			]
-		}.to_json,
-    headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"x-api-key": "NDAyODgyMDc3YjlhNjExOTAxN2JiYzAzODk5MzA2NzEtMTYzMDk0ODIwMi0w"
-		}
-	)
+  def initialize(_email)
+    @notify_email = _email
+  end
 
-    end
-end
-if __FILE__ == $0
-	n=Notification.new
-	p n.invoke
+  def invoke(_msg)
+    HTTParty.post("https://mail.surenotifyapi.com/v1/messages", 
+    body: {
+      subject: "#{_msg}",
+      fromName: "公車通知",
+      fromAddress: "joehwang.com@gmail.com",
+      content: "<html><body><h3>#{_msg}</h3></body></html>",
+      unsubscribedLink: "https://links.to.your.unsubscribe.link",
+      recipients: [
+        {
+          name: "#{@notify_email}",
+          address: "#{@notify_email}",
+          variables: {
+            UUID: "1234-56-78-9012"
+          }
+        }
+      ]
+    }.to_json,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "x-api-key": "NDAyODgyMDc3YjlhNjExOTAxN2JiYzAzODk5MzA2NzEtMTYzMDk0ODIwMi0w"
+      }
+    )
+  end
 end
